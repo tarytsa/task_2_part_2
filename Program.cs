94dd7918b420e6d38ba0147fe6408578ebaa4201
tarytsa <a.tarytsa@gmail.com>
2020-06-03 11:36:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anon_Methods
+{
+    delegate int MissingNum(int[] mass);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass1 = new int[] { 7,5, 10, 2, 4, 6, 8, 3, 9};
+
+            MissingNum missNum = delegate (int[] mass)
+            {
+                Array.Sort(mass);
+                for(int i = 0; i < mass.Length; i++)
+                {
+                    if (mass[i]!=i+1)
+                    {
+                        return i+1;
+                    }
+                }
+                return 10;
+            };
+            Console.WriteLine(missNum(mass1));
+        }
+    }
+}
